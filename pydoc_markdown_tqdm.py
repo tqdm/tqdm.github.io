a4b3f32af0211d31bae721ff316b6f35476cf071
Casper da Costa-Luis <tqdm@cdcl.ml>
2021-09-19 16:23:07 +0100

    misc code tidy

--- pydoc_markdown_tqdm.py
+++ pydoc_markdown_tqdm.py
@@ -2,14 +2,7 @@ from pydoc_markdown.contrib.processors.pydocmd import PydocmdProcessor
 import re
 from functools import partial
 
-compile = partial(re.compile, flags=re.M)
-RE_PARAM_ANY = compile(r"^(\w+\s{2,}:.*?)\*(.*?)$")
-RE_PARAM = compile(r"^(\w+)\s{2,}(:.*?)$")
-RE_H2 = compile(r"^(.+?)\n[-]{4,}$")
-RE_REPL = compile(r"^(>>>|\.\.\.)(.*?)$")
-RE_REPL_MLINE = compile(r"^(>>>|\.\.\.)(.*?)\n```\n```\n(>>>|\.\.\.)")
-RE_REPL_BLOCK = compile(r"^(```)(\n>>>)")
-RE_LONG_LINE = compile(r"\\\n\s*")
+sub = partial(re.sub, flags=re.M)
 
 
 class TqdmProcessor(PydocmdProcessor):
@@ -18,17 +11,17 @@ class TqdmProcessor(PydocmdProcessor):
             return
 
         # join long lines ending in escape (\)
-        node.docstring = RE_LONG_LINE.sub("", node.docstring)
+        node.docstring = sub(r"\\\n\s*", "", node.docstring)
         # escape literal `*`
-        node.docstring = RE_PARAM_ANY.sub(r"\1\\*\2", node.docstring)
+        node.docstring = sub(r"^(\w+\s{2,}:.*?)\*(.*?)$", r"\1\\*\2", node.docstring)
         # convert parameter lists to markdown list
-        node.docstring = RE_PARAM.sub(r"* __\1__*\2*  ", node.docstring)
+        node.docstring = sub(r"^(\w+)\s{2,}(:.*?)$", r"* __\1__*\2*  ", node.docstring)
         # convert REPL code blocks to code
-        node.docstring = RE_REPL.sub(r"```\n\1\2\n```", node.docstring)
-        node.docstring = RE_REPL_MLINE.sub(r"\1\2\n\3", node.docstring)
-        node.docstring = RE_REPL_MLINE.sub(r"\1\2\n\3", node.docstring)
-        node.docstring = RE_REPL_BLOCK.sub(r"\1python\2", node.docstring)
+        node.docstring = sub(r"^(>>>|\.\.\.)(.*?)$", r"```\n\1\2\n```", node.docstring)
+        node.docstring = sub(r"^(>>>|\.\.\.)(.*?)\n```\n```\n(>>>|\.\.\.)", r"\1\2\n\3", node.docstring)
+        node.docstring = sub(r"^(>>>|\.\.\.)(.*?)\n```\n```\n(>>>|\.\.\.)", r"\1\2\n\3", node.docstring)
+        node.docstring = sub(r"^(```)(\n>>>)", r"\1python\2", node.docstring)
         # hide <h2> from `nav`
-        node.docstring = RE_H2.sub(r"__\1__\n", node.docstring)
+        node.docstring = sub(r"^(.+?)\n[-]{4,}$", r"__\1__\n", node.docstring)
 
         return super()._process(node)
